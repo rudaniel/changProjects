5cedff9b194043241658ae00852392e4b3006468
yeetstatic <balitinna@gmail.com>
2021-10-17 16:54:27 -0400

    4:54 PM 10/17/2021

---
+++ Project3/src/project2/Roster.java
@@ -0,0 +1,262 @@
+package project2;
+
+/**
+*The Roster class is what stores our students and handles features.
+*The Roster class is able to add new students, remove students and can calculate tuition/payments.
+*@author Daniel Lopez, Manav Bali
+*/
+
+public class Roster {
+	private Student[] roster;
+	private int size; //keep track of the number of students in the roster
+	
+	/**
+	 * Looks through the roster[] array to see if the student is in the collection.
+	 * @param student that is being searched for.
+	 * @return index if found, -1 if not in collection.
+	 */
+	private int find(Student student) {
+		
+		int notFound = -1;
+		
+		for (int i = 0; i <= this.size-1; i++) { //works TESTED
+			
+			if(roster[i].equals(student) ) {
+				return i;
+			}
+			
+		}
+		return notFound;
+	} 
+	
+	/**
+	 * Increases the roster[] array by 4 without losing any students.
+	 */
+	private void grow() {
+		
+		Student[] copy = new Student[roster.length];
+		
+		for(int i =0; i <= size-1; i++) {
+			copy[i] = roster[i];
+		}
+		
+		this.roster = (new Student[roster.length+4]);
+		
+		for(int i =0; i <= size-1; i++) {
+			roster[i] = copy[i];
+		}
+		
+	}
+	
+	/**
+	 * Checks the current roster size and adds the student to the roster[] array.
+	 * If the size is too small, the array size gets increased.
+	 * If the size is 0 the array roster[] is initialized.
+	 * @param student that needs to be added.
+	 * @return true if student is added, false otherwise.
+	 */
+	public boolean add(Student student) { 
+		int currentSize = this.size;
+		if (currentSize == 0) {
+			this.roster = (new Student[4]);
+			roster[0] = student;
+			this.size  = 1;
+			return true;
+		}		
+		if (find(student) != -1) {
+			return false;			
+		}
+		if (currentSize == roster.length && find(student) == -1) {	
+			 grow();
+		}
+		roster[size] = student;
+		this.size ++;
+		return true;
+	} 
+	
+	/**
+	 * Checks if the student is in the roster. 
+	 * Removes that student and moves students after the removed back a spot in the array.
+	 * @param student that is being removed.
+	 * @return true if student is removed, false otherwise.
+	 */
+	public boolean remove(Student student) {
+	    int index = find(student);
+	    int notfound = -1;
+		
+		if (index == notfound) {
+			//System.out.println("Student is already in the roster.");
+			return false;
+		}
+		for(int i =index; i <=  size-1; i++) {
+			if(i != size-1) {
+				this.roster[i] =  this.roster[i+1];	
+			}
+			if(i == size-1) {
+				this.roster[i] = null;
+			}
+		}
+		//System.out.println("Student removed from the roster.");
+		this.size --;
+		return true; 
+	}
+	
+	/**
+	 * Checks if collection is empty and if not prints the entire albums[] array.
+	 */
+	public void print() {
+	
+		if(this.size==0) {
+			System.out.println("Student roster is empty!");
+			return;
+		}
+		System.out.println("* list of students in the roster **");
+		
+		for(int counter=0; counter<roster.length; counter++) {
+			if(roster[counter]!=null) {
+			System.out.println(roster[counter]);
+			}
+		}
+		System.out.println("* end of roster **");
+	
+	}
+	
+	
+
+	/**
+	 * Checks if collection is empty and if not prints the entire albums[] array with tuition amount.
+	 */
+	public void printC() { 
+		for(int counter=0; counter<roster.length; counter++) {
+			if(roster[counter]!=null&&roster[counter].getTuition()==0&&roster[counter].getPayment()==0) {
+			roster[counter].tuitionDue();
+			}
+		}
+	}
+	
+
+	/**
+	 * Sorts roster[] by Name and prints them.
+	 */
+   public void printN() {
+		
+		if(this.size==0) {
+			System.out.println("Student roster is empty!");
+			return;
+		}	
+		for(int counter=0; counter<roster.length;counter++) {
+			int minIndex=counter;
+			for(int secCounter=minIndex+1; secCounter<roster.length; secCounter++) {
+				if(roster[secCounter]!=null && roster[secCounter].getProfile().getName().charAt(0)<roster[minIndex].getProfile().getName().charAt(0)) {
+					minIndex= secCounter;
+				}
+				if(roster[secCounter]!=null && roster[secCounter].getProfile().getName().charAt(0)==roster[minIndex].getProfile().getName().charAt(0)) {
+					if(roster[secCounter].getProfile().getName().charAt(1)<roster[minIndex].getProfile().getName().charAt(1)) {
+						minIndex= secCounter;
+					}
+				}
+			}
+			Student temp= roster[minIndex];
+			roster[minIndex]=roster[counter];
+			roster[counter]=temp;
+		}
+		System.out.println("* list of students ordered by name **");	
+		for(int counter=0; counter<roster.length; counter++) {
+			if(roster[counter]!=null) {
+			System.out.println(roster[counter]);
+			}
+		}
+		System.out.println("* end of roster **");
+		
+	}
+
+   /**
+	 * Sorts roster[] by Payment Date and prints them.
+	 */
+	public void printT() {
+		if(this.size==0) {
+			System.out.println("Student roster is empty!");
+			return;
+		}
+		Student[] sortedDate= new Student[roster.length];
+		boolean paid=false;
+		for(int counter=0; counter<roster.length;counter++) {
+			if(roster[counter]!=null&&roster[counter].getPayment() > 0) {//will only had students who had made payments to the array
+				sortedDate[counter]=roster[counter]; //Adds them in the right order, no nulls	
+				paid=true;
+			}
+		}
+		if(!paid) {
+			System.out.println("Student roster is empty!");
+			return;
+		}
+		for(int counter=0; counter<sortedDate.length;counter++) {
+			int minIndex=counter;
+			for(int secCounter=minIndex+1; secCounter<sortedDate.length; secCounter++) {
+				if(sortedDate[secCounter]!=null&&sortedDate[minIndex]!=null&&sortedDate[secCounter].getDate().compareTo(sortedDate[minIndex].getDate())==-1) {
+					minIndex= secCounter;
+				}	
+			}
+			Student temp= sortedDate[minIndex];
+			sortedDate[minIndex]=sortedDate[counter];
+			sortedDate[counter]=temp;
+		}
+		System.out.println("* list of students made payments ordered by payment date **");
+		for(int counter=0; counter<sortedDate.length; counter++) {
+			if(sortedDate[counter]!=null) {
+			System.out.println(sortedDate[counter]);
+			}
+		}
+		System.out.println("* end of roster **");
+	}
+	
+	/**
+	 * Checks if the student is in the roster. 
+	 * Makes payment on behalf of student.
+	 * @param student that is making the payment.
+	 * @return true if payment successful, false otherwise.
+	 */
+	public boolean payment(Student student) {
+		int notFound=-1;
+		int index = find(student);
+		if(index==notFound) {
+			return false;
+		}
+		if(roster[index].getTuition()>=student.getPayment()) {
+			roster[index].payTuition(student.getPayment(),student.getDate());
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks if the International student is in the roster. 
+	 * Sets studyAbroad status of student.
+	 * @param student that is getting study abroad changed.
+	 * @return true if status change successful, false otherwise.
+	 */
+	public boolean abroadStatus(International student) {
+		int notFound=-1;
+		int index = find(student);
+		if(index==notFound)
+			return false;
+		if(roster[index].setStudyAbroad(student.getStatus()))
+			return true;
+		return false;
+	}	
+	
+	/**
+	 * Checks if the student is in the roster. 
+	 * Sets financial aid of student.
+	 * @param student that is getting financial aid.
+	 * @return true if aid given successfully, false otherwise.
+	 */
+	public String aid(Student student) {
+		int notFound=-1;
+		int index = find(student);
+		if(index==notFound)
+			return "Student not in the roster.";
+		return roster[index].giveAid(student.getPayment());
+	}	
+	
+}
---
+++ Project3/src/project2/Student.java
@@ -0,0 +1,215 @@
+package project2;
+
+import java.text.DecimalFormat;
+
+/**
+ * The Student Super class is where we create a student of type Student.
+ * There are getter and setter methods for the Student's properties (except class).
+ * Two subclass extend student.
+ * @author Manav Bali
+ * @author Daniel Lopez
+ */
+public class Student {
+
+	public static final int minFulltimeCredits = 12;
+	public static final double discountRate = 0.8;
+	public static final int maxFreeCredits = 16;
+	public static int UFEE = 3268;
+	private Profile profile;
+	private int credits;
+	private double tuition;
+	private double payment;
+	private Date date;
+	private boolean parttime; 
+
+	/**
+	 * Method extends subclasses and is used to calculate tuition.
+	 */
+	public void tuitionDue() {
+		
+	}
+	
+	/**
+	 * Method extends subclasses and is used to print desired output.
+	 */
+	@Override
+	public String toString() {
+		DecimalFormat format=new DecimalFormat("#,##0.00");
+		return profile+":"+credits+" credit hours"+":"+"tuition due:"+format.format(tuition)+":"+"total payment:"+format.format(payment)+":"+"last payment date: "+(date==null? "--/--/--":date)+":";
+	}
+	
+	/**
+	 * Creates an instance of student given the correct parameters.
+	 * @param profile profile of student.
+	 * @param payment payment by student.
+	 * @param date date of payment.
+	 */
+	public Student(Profile profile, double payment, Date date) {
+		this.profile = profile;
+		this.payment=payment;
+		this.date=date;
+	}
+	
+	/**
+	 * Creates an instance of student given the correct parameters.
+	 * @param profile profile of student.
+	 * @param credits credits of student.
+	 */
+	public Student(Profile profile, int credits) {
+		this.profile = profile;
+		this.credits=credits;
+		if(credits<minFulltimeCredits) {
+			this.parttime=true;
+		}
+	}
+
+	/**
+	 * Creates an instance of student given the correct parameters.
+	 * @param profile profile of student.
+	 * @param payment payment by student.
+	 */
+	public Student(Profile profile, double payment) {
+		this.profile = profile;
+		this.payment=payment;
+	}
+
+
+	
+	/**
+	 * Creates an instance of student given the correct parameters.
+	 * @param profile profile of student.
+	 */
+	public Student(Profile profile) {
+		this.profile=profile;
+	}
+	
+	/**
+	 * Updates tuition based on payment. 
+	 * @param amount amount being payed.
+	 * @param date date of payment.
+	 * @return true if paid, false otherwise.
+	 */
+	public boolean payTuition(double amount, Date date){
+		this.date=date;
+		this.payment=this.payment+amount;
+		this.tuition=this.tuition-amount;
+		return true;
+	}
+
+	/**
+	 * Grabs the current instance of Student and comapres to other student.
+	 * @param student being compared
+	 * @return true if equal, false otherwise. 
+	 */
+	public boolean equals(Student student) {
+		return this.profile.equals(student.profile);
+	}
+	
+	/**
+	 * Getter method to return payment.
+	 * @return payment.
+	 */
+	public double getPayment() {
+		return payment;
+	}
+	
+	/**
+	 * Setting method to update total payments.
+	 * @param payment being made.
+	 */
+	public void setPayment(double payment) {
+		this.payment = payment;
+	}
+	
+	/**
+	 * Getter method to return credits.
+	 * @return credits.
+	 */
+	public int getCredits() {
+		return credits;
+	}
+	
+	/**
+	 * Setting method to update credits.
+	 * @param credits to be updated.
+	 */
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+	
+	/**
+	 * Getter method to return tuition.
+	 * @return tuition.
+	 */
+	public double getTuition() {
+		return tuition;
+	}
+	
+	/**
+	 * Setting method to update tuition.
+	 * @param tuition to be updated.
+	 */
+	public void setTuition(double tuition) {
+		this.tuition = tuition;
+	}
+	
+	/**
+	 * Getter method to return date.
+	 * @return date.
+	 */
+	public Date getDate() {
+		return date;
+	}
+	
+	/**
+	 * Getter method to return Profile.
+	 * @return profile.
+	 */
+	public Profile getProfile() {
+		return profile;
+	}
+	
+	/**
+	 * Setting method to update credits.
+	 * @param date to be updated.
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	/**
+	 * Getter method to return parttime.
+	 * @return parttime
+	 */
+	public boolean isParttime() {
+		return parttime;
+	}
+
+	/**
+	 * Setting method to update part time.
+	 * @param parttime to be updated.
+	 */
+	public void setParttime(boolean parttime) {
+		this.parttime = parttime;
+	}
+
+	/**
+	 * Declares a international student as abroad and limits credits if needed.
+	 * @param studyAbroad studyAbroad status.
+	 * @return true if studyAbroad changed, false otherwise.
+	 */
+	public boolean setStudyAbroad(boolean studyAbroad) {
+		return false;
+	}
+	
+	/**
+	 * Applies student's aid to their tuition.
+	 * Aid can't be over tuition total.
+	 * @param amount of aid.
+	 * @return applied if aid applied to tuition, error statement otherwise.
+	 */
+	public String giveAid(double amount) {
+		return "Not a resident student.";
+	}
+
+}
