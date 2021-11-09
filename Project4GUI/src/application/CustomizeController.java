09a15fb352d18b42758dcacd6dccb9eebb273b7b
yeetstatic <balitinna@gmail.com>
2021-11-09 06:31:04 -0500

    w

--- Project4GUI/src/application/CustomizeController.java
+++ Project4GUI/src/application/CustomizeController.java
@@ -1,5 +1,7 @@
 package application;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -7,6 +9,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
@@ -14,13 +17,15 @@ import javafx.scene.layout.Pane;
 
 public class CustomizeController {
 	
+	private MainController mainController;
 	ObservableList<String> sizes= FXCollections.observableArrayList("Small","Medium","Large");
 	ObservableList<String> Toppings= FXCollections.observableArrayList("Beef","Black Olives","Cheese","Chicken","Green Pepper","Ham","Mushroom","Onion","Pepperoni","Pineapple","Sausage");
+	ObservableList<String> selectedToppings;
 	@FXML
-	public ChoiceBox size;
+	public ChoiceBox<String> size;
 	
 	@FXML
-	public ListView sTop;
+	public ListView sTop, aTop;
 	
 	@FXML
 	public Pane pizzaImg;
@@ -29,26 +34,100 @@ public class CustomizeController {
 	public Label pizzaLbl;
 	
 	@FXML
+	public TextField price;
+	
+	@FXML
 	public void initialize() {
+		String small="Small";
+		String medium="Medium";
+		String large="Large";
 		size.setItems(sizes);
-		size.setValue("Small");
-		sTop.getItems().addAll(Toppings);
-		sTop.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		size.setValue(small);
+		aTop.getItems().addAll(Toppings);
+		size.valueProperty().addListener(new ChangeListener<String>() {
+	        @Override public void changed(ObservableValue ov, String oldS, String newS) {
+	        	Double curPrice= Double.parseDouble(price.getText());
+	        	String blank="";
+	        	int big=4;
+	        	int little=2;
+	        	String temp="";
+	        	if(newS.equals(small)){
+	        		if(oldS.equals(medium))
+	        			temp=(curPrice-little)+blank;
+	        		if(oldS.equals(large))
+	        			temp=(curPrice-big)+blank;
+	        	}
+	        	else if(newS.equals(large)) {
+	        		if(oldS.equals(medium))
+	        			temp=(curPrice+little)+blank;
+	        		if(oldS.equals(small))
+	        			temp=(curPrice+big)+blank;
+	        	}
+	        	else {
+	        		if(oldS.equals(large))
+	        			temp=(curPrice-little)+blank;
+	        		if(oldS.equals(small))
+	        			temp=(curPrice+little)+blank;
+	        	}
+	        	price.setText(temp);
+	        }    
+	    });
 	}
 	
-	public void set(int pizza) {
+	public void set() {
+		int pizza=mainController.getPizza();
 		Image image;
+		String text;
 		if(pizza==0){
 			image= new Image("/deluxePizza.png");
+			text="Deluxe";
+			selectedToppings=FXCollections.observableArrayList("Black Olives","Cheese","Chicken","Green Pepper","Mushroom");
+			Toppings= FXCollections.observableArrayList("Beef","Ham","Onion","Pepperoni","Pineapple","Sausage");	
 		}
 		else if(pizza==1) {
 			image= new Image("/hawaiianPizza.png");
+			text="Hawaiian";
+			selectedToppings=FXCollections.observableArrayList("Ham","Pineapple");
+			Toppings= FXCollections.observableArrayList("Beef","Black Olives","Cheese","Chicken","Green Pepper","Mushroom","Onion","Pepperoni","Sausage");	
 		}
 		else{
 			image= new Image("/pepperoniPizza.png");
+			text="Pepperoni";
+			selectedToppings=FXCollections.observableArrayList("Pepperoni");
+			Toppings= FXCollections.observableArrayList("Beef","Black Olives","Cheese","Chicken","Green Pepper","Ham","Mushroom","Onion","Pineapple","Sausage");
 		}
 		BackgroundImage back= new BackgroundImage(image,null,null,null,null);
 		pizzaImg.setBackground(new Background(back));
+		pizzaLbl.setText(text);
+		aTop.getItems().clear();
+		aTop.getItems().addAll(Toppings);
+		sTop.getItems().addAll(selectedToppings);
+	}
+
+	public void setMainController(MainController controller) {
+		mainController=controller;
+		set();
+		
+	}
+	
+	public void addTop() {
+		if(!aTop.getSelectionModel().isEmpty()) {
+			int index=aTop.getSelectionModel().getSelectedIndex();
+			sTop.getItems().add(aTop.getItems().get(index).toString());
+			aTop.getItems().remove(index);
+		}
+	}
+	
+	public void removeTop() {
+		if(!sTop.getSelectionModel().isEmpty()) {
+			int index=sTop.getSelectionModel().getSelectedIndex();
+			aTop.getItems().add(sTop.getItems().get(index).toString());
+			sTop.getItems().remove(index);
+		}
+	}
+	
+	public void addOrder() {
+		
 	}
 	
 }
--- Project4GUI/src/application/MainController.java
+++ Project4GUI/src/application/MainController.java
@@ -3,20 +3,24 @@ package application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainController {
-	
+	private int pizza=0;
 	public void deluxeP() {
+		this.pizza=0;
 		customize();
 	}
 	
 	public void hawaiianP() {
-		
+		this.pizza=1;
+		customize();
 	}
 	
 	public void pepperoniP() {
-		
+		this.pizza=2;
+		customize();
 	}
 	
 	public void orders() {
@@ -30,14 +34,22 @@ public class MainController {
 	public void customize() {
 		try {
 			Stage stage= new Stage();
-			Parent root = FXMLLoader.load(getClass().getResource("CustomizePizzaView.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("CustomizePizzaView.fxml"));
+			Parent root = loader.load();
 			Scene scene = new Scene(root,500,610);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			stage.setScene(scene);
 			stage.show();
+			CustomizeController custom= loader.getController();
+			custom.setMainController(this);
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public int getPizza() {
+		return pizza;
 	}
 	
 }
